1ba35c209e0cf020c763d5a09b4e2a53f8df2ebd
ViniciusLedro <158050228+ViniciusLedro@users.noreply.github.com>
2025-09-04 14:33:09 -0300

    Add files via upload

---
+++ docs/src/components/AnimatedBackground/AnimatedBackground.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./AnimatedBackgorund.css";
+
+const AnimatedBackgorund: React.FC = () => {
+  return (
+    <div id="space">
+      <div className="stars"></div>
+      <div className="stars"></div>
+      <div className="stars"></div>
+      <div className="stars"></div>
+      <div className="stars"></div>
+    </div>
+  );
+};
+
+export default AnimatedBackgorund;
